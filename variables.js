667b3611f0773eb398ee75ddee770bd9cbeedbed
Mirty <mirtybmx@gmail.com>
2023-02-17 22:05:01 +0000

    Se agregó el archivo variables.js

---
+++ variables.js
@@ -0,0 +1,94 @@
+
+let nombreDeUsuario = "Jaimito"; // 0 "Jaimito"
+
+console.log(nombreDeUsuario);
+
+//const apellido = "Pérez";
+
+nombreDeUsuario = "Pepito";
+
+console.log(nombreDeUsuario);
+
+
+//console.log(apellido);
+
+//apellido = 'Cuis'; // no se puede porque está con 'const'
+
+
+
+// TIPOS DE DATOS
+
+
+// ANCHOR ---> Strings = Cadena de texto o cadena de caracteres
+
+let frase = 'Hola Mundo';
+
+
+edad = 30;
+
+console.log(typeof edad);
+
+// ANCHOR ---> Enteros
+
+edad = 30; 
+
+// ANCHOR ---> Decimales (con '.')
+
+let precio = 13.5; 
+
+// ANCHOR ---> Booleanos = boolean
+
+let estaLogeado = false; // ó 'true'
+
+
+// ANCHOR ---> Undefined = No tiene valor pero es un dato
+
+let email = undefined;
+
+console.log(email);
+
+
+// Null = La variable no tiene contenido
+// NaN = 
+
+console.log('Hola' / 5);
+
+
+
+// OPERADORES
+
+let nombre = 'Juancito';
+let apellido = 'López';
+
+console.log(nombre + ' ' + apellido);
+
+let nombreCompleto = nombre + ' ' + apellido;
+
+let saludo = 'Hola' + ' ' + nombre + ' ' + 'como estas?';
+
+console.log(saludo);
+
+let saludo2 = `Hola ${nombre} ${apellido} como estas?`;
+
+console.log(saludo2);
+
+let num1 = 5;
+let num2 = 23;
+
+let suma = num1 + num2;
+let mult = num1 * num2;
+let div = num1 / num2;
+let resta = num1 - num2;
+let modulo = num1 % num2; // ---> Me devuelve el resto
+
+console.log(suma);
+console.log(mult);
+console.log(div);
+console.log(resta);
+console.log(modulo);
+
+
+// OPERADORES DE COMPARACIÓN
+
+// OPERSDORES LÓGICOS
+
